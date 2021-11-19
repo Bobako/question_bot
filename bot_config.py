17b6e4455905cde555306b3e91beeb560275e7c0
Bobako <52820434+Bobako@users.noreply.github.com>
2021-11-19 10:23:55 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,8 +1,8 @@
 # Токен бота, полученный в BotFather
-TOKEN = "2123867529:AAEhpbHaPXT-DXlVmoZZ1Rrrsc8HwRNN2Q0"
+TOKEN = ""
 
 # Токен bitrix24
-BITRIX_URL = "https://elbamotors.bitrix24.ru/rest/1/sr3mby9hq0a62ibq/"
+BITRIX_URL = ""
 
 # Список администраторов
 admins = ["bobak00"]
